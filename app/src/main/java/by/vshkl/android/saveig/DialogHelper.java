48cda67f38dfea32449132dfc4156b444b5448b7
jgug <jgugn1@gmail.com>
2017-03-21 11:06:40 +0300

    Change dialog library

--- app/src/main/java/by/vshkl/android/saveig/DialogHelper.java
+++ app/src/main/java/by/vshkl/android/saveig/DialogHelper.java
@@ -5,6 +5,8 @@ import android.support.annotation.NonNull;
 
 import com.afollestad.materialdialogs.DialogAction;
 import com.afollestad.materialdialogs.MaterialDialog;
+import com.github.javiersantos.materialstyleddialogs.MaterialStyledDialog;
+import com.github.javiersantos.materialstyleddialogs.enums.Style;
 
 import permissions.dispatcher.PermissionRequest;
 
@@ -12,11 +14,13 @@ class DialogHelper {
 
     static void showWriteExternalStorageRationaleDialog(final Context context,
                                                         final PermissionRequest request) {
-        new MaterialDialog.Builder(context)
-                .title(R.string.permission_rationale_title)
-                .content(R.string.permission_rationale_message)
-                .positiveText(R.string.permission_rationale_allow)
-                .negativeText(R.string.permission_rationale_deny)
+        new MaterialStyledDialog.Builder(context)
+                .setStyle(Style.HEADER_WITH_TITLE)
+                .setTitle(R.string.permission_rationale_title)
+                .setDescription(R.string.permission_rationale_message)
+                .setHeaderColor(R.color.colorPrimary)
+                .setPositiveText(R.string.permission_rationale_allow)
+                .setNegativeText(R.string.permission_rationale_deny)
                 .onPositive(new MaterialDialog.SingleButtonCallback() {
                     @Override
                     public void onClick(@NonNull MaterialDialog dialog, @NonNull DialogAction which) {
@@ -29,6 +33,7 @@ class DialogHelper {
                         request.cancel();
                     }
                 })
+                .build()
                 .show();
     }
 }
